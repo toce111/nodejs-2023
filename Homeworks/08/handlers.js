65f02723f85c7c7ed738a76e97b72fd35e8da99f
Antonio <nestorovantonio@icloud.com>
2023-03-15 21:09:40 +0100

    still not fixed.

--- Homeworks/08/handlers.js
+++ Homeworks/08/handlers.js
@@ -1,16 +1,22 @@
+const fs = require('fs');
+
 const home = (req, res) => {
   res.send("welcome to the student page, proceed to /student.");
 };
 const students = (req, res) => {
   const { name, average } = req.body;
 
-  let studentsData = [];
-  if (fs.existsSync("./students.json")) {
-    const data = fs.readFileSync("./students.json");
+  let studentsData;
+  if (fs.existsSync("students.json")) {
+    const data = fs.readFile("students.json", 'utf8', (err, data) => {
+      if (err) return err;
+      return data
+    });
+    console.log(data);
     studentsData = JSON.parse(data);
   }
   studentsData.push({ name, average });
-  fs.writeFileSync("./students.json", JSON.stringify(studentsData));
+  fs.writeFile("students.json", JSON.stringify(studentsData));
 
   res.send("Student was added successfully!");
 };
