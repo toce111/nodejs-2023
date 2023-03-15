d9e0fcd4965ac8acb6b8d0acb631a9811f3408c9
Antonio <nestorovantonio@icloud.com>
2023-03-15 21:23:50 +0100

    fixed handlers.js

--- Homeworks/08/handlers.js
+++ Homeworks/08/handlers.js
@@ -3,22 +3,28 @@ const fs = require('fs');
 const home = (req, res) => {
   res.send("welcome to the student page, proceed to /student.");
 };
+
 const students = (req, res) => {
   const { name, average } = req.body;
 
   let studentsData;
+
   if (fs.existsSync("students.json")) {
-    const data = fs.readFile("students.json", 'utf8', (err, data) => {
-      if (err) return err;
-      return data
-    });
+    const data = fs.readFileSync("students.json", 'utf8');
     console.log(data);
     studentsData = JSON.parse(data);
   }
-  studentsData.push({ name, average });
-  fs.writeFile("students.json", JSON.stringify(studentsData));
 
-  res.send("Student was added successfully!");
+  studentsData.push({ name, average });
+  
+  fs.writeFile("students.json", JSON.stringify(studentsData), (err) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send("Error writing to file");
+    } else {
+      res.send("Student was added successfully!");
+    }
+  });
 };
 
 module.exports = {
