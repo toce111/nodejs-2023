2da1578f37e4158deb357bd0e1e6ae0d69436105
Antonio <nestorovantonio@icloud.com>
2023-04-12 20:06:16 +0200

    protected

--- ws1/pkg/db/index.js
+++ ws1/pkg/db/index.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const init = () => {
-    const dsn = "mongodb+srv://nestorovantonio:KxsQkZLToOJBmhOy@javascriptakademija.3ida2ea.mongodb.net/JavascriptAcademy?retryWrites=true&w=majority";
     mongoose.connect(dns);
 }
 
